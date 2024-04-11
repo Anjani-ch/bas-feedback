93f12741d8b04c0726c42f1ad746b389c6d9292e
Anjani Chapparapu <anjani.chapparapu@bas.no>
2024-04-11 14:55:33 +0200

    fix: add Ca certificate for db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,8 @@
 import { PostgresJsDatabase, drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
+import * as fs from 'fs'
+import path from 'path'
 
 declare global {
 	var db: PostgresJsDatabase<typeof schema> | undefined
@@ -9,7 +11,16 @@ declare global {
 let db: PostgresJsDatabase<typeof schema>
 
 if (process.env.NODE_ENV === 'production') {
-	db = drizzle(postgres(process.env.DATABASE_URL!))
+	db = drizzle(
+		postgres(process.env.DATABASE_URL!, {
+			ssl: {
+				require: true,
+				ca: fs.readFileSync(
+					path.join(process.cwd(), 'certs', 'ca-certificate.crt')
+				),
+			},
+		})
+	)
 } else {
 	if (!global.db) {
 		global.db = drizzle(postgres(process.env.DATABASE_URL!))
